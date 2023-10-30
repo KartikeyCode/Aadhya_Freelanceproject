7c0756ab22e4e83131e7f2660a6e6e94a6d5df4d
Kartikey Seth <codekartikey@gmail.com>
2023-10-30 21:24:10 +0530

    fix contact page responsive macbook

--- resume/app/Contact/page.js
+++ resume/app/Contact/page.js
@@ -13,14 +13,14 @@ export default function Contact(){
 
             <div className="flex ">
 
-                <div className="flex-col left mt-52 ml-28">
+                <div className="flex-col left xl:mt-36 2xl:mt-52 ml-28">
 
                     <h1 className="text-[#212121] font-NewSpirit text-7xl "> Contact Me. </h1>
 
-                    <h2 className="text-[#373737] font-SegoeUI text-2xl w-[33rem] mt-4"> 
+                    <h2 className="text-[#373737] font-SegoeUI xl:text-xl 2xl:text-2xl w-[33rem] mt-4"> 
                         
                         If you’d like to reach out, you can do so in the contact box here. You can also find other contacts listed below.
-                        <br/> Looking forward to hearing from you! 
+                         Looking forward to hearing from you! 
                         <br/>
                         
                         <br/>Email: aadhya05may@gmail.com
@@ -29,16 +29,16 @@ export default function Contact(){
                         </h2>
 
                         <Image src={arrow} className="ml-96 -mt-10" />
-                        <Image src={head} className="-mt-[10.15rem] -ml-20" />
+                        <Image src={head} className="xl:-mt-[11.9rem] 2xl:-mt-[10.15rem] -ml-20" />
 
 
                 </div>
 
-                <div className="flex flex-col  border-4 border-black w-[32rem] h-[39rem] mt-52 ml-11">
+                <div className="flex flex-col  border-4 border-black w-[32rem]  xl:h-[39rem] xl:mt-36 2xl:mt-52 ml-11">
 
                     <Image src={circles} className="object-contain self-start mt-2 ml-6" />
                     
-                    <div className="line bg-black w-[31.5rem] h-1 mt-2">  </div>
+                    <div className="line bg-black 2xl:w-[31.5rem] h-1 mt-2">  </div>
                     
                     <h1 className="font-NewSpirit text-[#212121] text-6xl ml-16 mt-20"> Say Hi! </h1>
                     
@@ -54,7 +54,7 @@ export default function Contact(){
 
                 </div>
 
-                <Image src={cursor} className="object-contain ml-10 mt-80" />
+                <Image src={cursor} className="object-contain xl:ml-5 2xl:ml-10 mt-80" />
 
 
             </div>
